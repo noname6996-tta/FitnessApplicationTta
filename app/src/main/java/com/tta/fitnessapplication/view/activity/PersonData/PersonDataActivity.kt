b1c79a73a514f96a5a578562c5e28302270f967b
Trần  Thế Anh <theanh682001@gmail.com>
2023-04-10 22:53:54 +0700

    ok

---
+++ app/src/main/java/com/tta/fitnessapplication/view/activity/PersonData/PersonDataActivity.kt
@@ -0,0 +1,12 @@
+package com.tta.fitnessapplication.view.activity.PersonData
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.tta.fitnessapplication.R
+
+class PersonDataActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_person_data)
+    }
+}
