5cd38a6addf9a85ba49757f0b80845c21bee4ffd
Tran The Anh <tran.the.anh@vinicorp.com>
2023-06-20 17:02:20 +0700

    add meal planner

---
+++ app/src/main/java/com/tta/fitnessapplication/view/activity/MealTracker/MealTrackerActivity.kt
@@ -0,0 +1,12 @@
+package com.tta.fitnessapplication.view.activity.MealTracker
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.tta.fitnessapplication.R
+
+class MealTrackerActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_meal_tracker)
+    }
+}
