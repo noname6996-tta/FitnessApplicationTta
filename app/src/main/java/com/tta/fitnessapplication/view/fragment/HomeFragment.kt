7c056313daba4c93fbd4575ffdb0f6a2b4fd27d9
Trần  Thế Anh <theanh682001@gmail.com>
2023-07-13 22:03:58 +0700

    update HomeFragment

--- app/src/main/java/com/tta/fitnessapplication/view/fragment/HomeFragment.kt
+++ app/src/main/java/com/tta/fitnessapplication/view/fragment/HomeFragment.kt
@@ -6,30 +6,43 @@ import android.graphics.Color
 import android.graphics.Typeface
 import android.graphics.drawable.ClipDrawable
 import android.util.Log
+import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.Lifecycle
+import androidx.core.content.ContextCompat
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.github.mikephil.charting.animation.Easing
 import com.github.mikephil.charting.data.PieData
 import com.github.mikephil.charting.data.PieDataSet
 import com.github.mikephil.charting.data.PieEntry
 import com.github.mikephil.charting.formatter.PercentFormatter
 import com.github.mikephil.charting.utils.MPPointF
+import com.google.android.material.snackbar.Snackbar
 import com.tta.fitnessapplication.R
 import com.tta.fitnessapplication.data.repository.RepositoryApi
 import com.tta.fitnessapplication.data.utils.Constant
+import com.tta.fitnessapplication.data.utils.Constant.DATE.fullDateFormatter
 import com.tta.fitnessapplication.databinding.FragmentHomeBinding
+import com.tta.fitnessapplication.view.activity.HistoryActivity.HistoryActivity
+import com.tta.fitnessapplication.view.activity.HistoryActivity.HistoryAdapter
+import com.tta.fitnessapplication.view.activity.HistoryActivity.HistoryViewModel
 import com.tta.fitnessapplication.view.activity.MainActivity.MainViewModel
 import com.tta.fitnessapplication.view.activity.MainActivity.MainViewModelFactory
 import com.tta.fitnessapplication.view.activity.SleepTracker.SleepTrackerActivity
 import com.tta.fitnessapplication.view.activity.calortracker.CalorTrackerActivity
 import com.tta.fitnessapplication.view.activity.watertracker.WaterTrackerActivity
 import com.tta.fitnessapplication.view.base.BaseFragment
+import java.time.LocalDate
+import java.util.Calendar
 
 class HomeFragment : BaseFragment<FragmentHomeBinding>() {
     private lateinit var mImageDrawable: ClipDrawable
     private lateinit var mainViewModel: MainViewModel
+    private var viewModelHistory = HistoryViewModel()
+    private val today = LocalDate.now()
+    private val eventsAdapter = HistoryAdapter()
     private lateinit var loginPreferences: SharedPreferences
     private lateinit var loginPrefsEditor: SharedPreferences.Editor
     override fun getDataBinding(): FragmentHomeBinding {
@@ -38,6 +51,7 @@ class HomeFragment : BaseFragment<FragmentHomeBinding>() {
 
     override fun addObservers() {
         super.addObservers()
+        //
         val viewModel = HistoryViewModelGoogleData()
         viewModel.getData()
         viewModel.listStepsCount.observe(viewLifecycleOwner) {
@@ -62,6 +76,51 @@ class HomeFragment : BaseFragment<FragmentHomeBinding>() {
         viewModel.message.observe(viewLifecycleOwner) {
             Toast.makeText(requireContext(), it, Toast.LENGTH_SHORT).show()
         }
+        //
+        val repositoryApi = RepositoryApi()
+        val viewModelFactory = MainViewModelFactory(repositoryApi)
+        mainViewModel = ViewModelProvider(this, viewModelFactory)[MainViewModel::class.java]
+        loginPreferences = requireActivity().getSharedPreferences(
+            Constant.LOGIN_PREFS,
+            AppCompatActivity.MODE_PRIVATE
+        )
+        loginPrefsEditor = loginPreferences.edit();
+        var token = loginPreferences.getString(Constant.EMAIL_USER, "").toString()
+        var idUser = loginPreferences.getString(Constant.PREF.IDUSER, "").toString()
+        if (token != "") {
+            mainViewModel.getUserData(token)
+        }
+        mainViewModel.dataExercise.observe(requireActivity()) {
+            if (it.isSuccessful) {
+                var dataProfile = mainViewModel.dataExercise.value?.body()?.data
+                Log.e("dataProfile", dataProfile.toString())
+                binding.textView2.text =
+                    "${dataProfile!![0].firstname} ${dataProfile!![0].lastname}"
+            } else {
+                Log.e("tta", it.errorBody().toString())
+            }
+        }
+        //
+        viewModelHistory.getListHistoryByDate(idUser, fullDateFormatter.format(today))
+
+        viewModelHistory.message.observe(this) {
+            binding.tvNoDataRecycle.visibility = View.VISIBLE
+            binding.tvNoDataRecycle.text = it
+        }
+        viewModelHistory.list.observe(this) {
+            Log.e("this", it.toString())
+
+            if (it != null) {
+                binding.tvNoDataRecycle.visibility = View.GONE
+                eventsAdapter.events.clear()
+                for (i in 0 until it.size) {
+                    if (i <= 2) {
+                        eventsAdapter.events.add(it[i])
+                    }
+                }
+                eventsAdapter.notifyDataSetChanged()
+            }
+        }
     }
 
     override fun addEvent() {
@@ -79,31 +138,36 @@ class HomeFragment : BaseFragment<FragmentHomeBinding>() {
         binding.cardViewEat.setOnClickListener {
             startActivity(Intent(activity, CalorTrackerActivity::class.java))
         }
-        val repositoryApi = RepositoryApi()
-        val viewModelFactory = MainViewModelFactory(repositoryApi)
-        mainViewModel = ViewModelProvider(this, viewModelFactory)[MainViewModel::class.java]
-        loginPreferences = requireActivity().getSharedPreferences(
-            Constant.LOGIN_PREFS,
-            AppCompatActivity.MODE_PRIVATE
-        )
-        loginPrefsEditor = loginPreferences.edit();
-        var token = loginPreferences.getString(Constant.EMAIL_USER, "").toString()
-        if (token != "") {
-            mainViewModel.getUserData(token)
+
+        binding.tvSeeMoreHistory.setOnClickListener {
+            startActivity(Intent(activity, HistoryActivity::class.java))
         }
-        mainViewModel.dataExercise.observe(requireActivity()) {
-            if (it.isSuccessful) {
-                var dataProfile = mainViewModel.dataExercise.value?.body()?.data
-                Log.e("dataProfile", dataProfile.toString())
-                binding.textView2.text =
-                    "${dataProfile!![0].firstname} ${dataProfile!![0].lastname}"
-            } else {
-                Log.e("tta", it.errorBody().toString())
-            }
+
+        binding.imgNotifiHome.setOnClickListener {
+            // go to noti activity
         }
     }
 
     override fun initView() {
+
+        mImageDrawable = binding.imgToScroll.drawable as ClipDrawable
+        mImageDrawable.level = 5000
+
+        initCalender()
+
+        initPieChart()
+
+        initRecycleViewHistory()
+    }
+
+    private fun initRecycleViewHistory() {
+        binding.recLatesProgess.apply {
+            layoutManager = LinearLayoutManager(requireContext(), RecyclerView.VERTICAL, false)
+            adapter = eventsAdapter
+        }
+    }
+
+    private fun initPieChart() {
         // on below line we are setting user percent value,
         // setting description as enabled and offset for pie chart
         binding.pieChart.setUsePercentValues(true)
@@ -183,10 +247,55 @@ class HomeFragment : BaseFragment<FragmentHomeBinding>() {
 
         // loading chart
         binding.pieChart.invalidate()
+    }
 
-        //
-        mImageDrawable = binding.imgToScroll.drawable as ClipDrawable
-        mImageDrawable.level = 5000
-        //
+    private fun initCalender() {
+        var arrayDay = ArrayList<String>()
+        val calendar = Calendar.getInstance()
+        // Set the calendar to the current week
+        calendar.set(Calendar.DAY_OF_WEEK, Calendar.MONDAY)
+        // Print each day number of the week
+        for (i in 1..7) {
+            println(calendar.get(Calendar.DAY_OF_MONTH))
+            arrayDay.add(calendar.get(Calendar.DAY_OF_MONTH).toString())
+            calendar.add(Calendar.DAY_OF_WEEK, 1) // Move to the next day
+        }
+        val dayNumber = calendar.get(Calendar.DAY_OF_MONTH).toString()
+        binding.tvDay1.text = arrayDay[0]
+        binding.tvDay2.text = arrayDay[1]
+        binding.tvDay3.text = arrayDay[2]
+        binding.tvDay4.text = arrayDay[3]
+        binding.tvDay5.text = arrayDay[4]
+        binding.tvDay6.text = arrayDay[5]
+        binding.tvDay7.text = arrayDay[6]
+        when (dayNumber) {
+            arrayDay[0] -> {
+                binding.tvDay1.setTextColor(ContextCompat.getColor(requireContext(), R.color.text))
+            }
+
+            arrayDay[1] -> {
+                binding.tvDay2.setTextColor(ContextCompat.getColor(requireContext(), R.color.text))
+            }
+
+            arrayDay[2] -> {
+                binding.tvDay3.setTextColor(ContextCompat.getColor(requireContext(), R.color.text))
+            }
+
+            arrayDay[3] -> {
+                binding.tvDay4.setTextColor(ContextCompat.getColor(requireContext(), R.color.text))
+            }
+
+            arrayDay[4] -> {
+                binding.tvDay5.setTextColor(ContextCompat.getColor(requireContext(), R.color.text))
+            }
+
+            arrayDay[5] -> {
+                binding.tvDay6.setTextColor(ContextCompat.getColor(requireContext(), R.color.text))
+            }
+
+            arrayDay[6] -> {
+                binding.tvDay7.setTextColor(ContextCompat.getColor(requireContext(), R.color.text))
+            }
+        }
     }
 }
