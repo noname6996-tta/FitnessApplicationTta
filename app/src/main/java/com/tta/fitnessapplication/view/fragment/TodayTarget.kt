57311b52cf6a2698da69ea9c73521b5ff6437ed7
Trần  Thế Anh <theanh682001@gmail.com>
2023-08-26 23:54:52 +0700

    do today target fragment

--- app/src/main/java/com/tta/fitnessapplication/view/fragment/TodayTarget.kt
+++ app/src/main/java/com/tta/fitnessapplication/view/fragment/TodayTarget.kt
@@ -1,4 +1,10 @@
 package com.tta.fitnessapplication.view.fragment
 
-class TodayTarget {
+import com.tta.fitnessapplication.databinding.FragmentTodayTargetBinding
+import com.tta.fitnessapplication.view.base.BaseFragment
+
+class TodayTarget : BaseFragment<FragmentTodayTargetBinding>() {
+    override fun getDataBinding(): FragmentTodayTargetBinding {
+        return FragmentTodayTargetBinding.inflate(layoutInflater)
+    }
 }
